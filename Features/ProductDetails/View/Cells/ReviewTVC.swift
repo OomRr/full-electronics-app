5415ff7e53173cba30913c3bc698758eb4c740fa
Omar Abo zaid <120683753+OomRr@users.noreply.github.com>
2026-05-16 01:49:27 +0300

    finished design except
    except for adding cells to the tableView and collectionView

---
+++ Features/ProductDetails/View/Cells/ReviewTVC.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewTVC.swift
+//  Full elcetronics store
+//
+//  Created by Omar on 15/05/2026.
+//
+
+import UIKit
+
+class ReviewTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
